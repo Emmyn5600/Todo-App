b21e7112c718297b8145c29cfb3cbdc7c53b94fd
Addis Belete <addis.bel@gmail.com>
2021-06-16 15:22:58 +0300

    Add a method to display data

--- index.js
+++ index.js
@@ -2,42 +2,61 @@ window.onload = function () {
 	const title = document.getElementById('title');
 	const description = document.getElementById('description')
 	const duedate = document.getElementById('duedate')
-	const priority = document.getElementById('priority')
+
 	const btn = document.getElementsByClassName('button')
 	const todoItems = [];
 	const form = document.querySelector('form')
 
-	const Todo = function (_title, _description, _duedate, _priority) {
+	const todo = function (_title, _description, _duedate, _priority) {
 		this._title = _title;
 		this._description = _description;
 		this._duedate = _duedate;
-		this._priority = _priority;
+		this.checked = false;
+		this.id = Date.now();
+
 
 	}
 	function clearField() {
 		title.value = '';
 		description.value = "";
 		duedate.value = '';
-		priority.value = '';
+
 
 	}
 
+	const displayData = function (todo) {
+		const list = document.getElementById('content');
+		const isChecked = todo.checked ? 'done' : '';
+		const node = document.createElement('li');
+		node.setAttribute('class', `todo-item ${isChecked}`);
+		node.setAttribute('data-key', todo.id);
+		node.innerHTML = `
+
+<span>Title:${todo.title}</span><br>
+<span>${todo.description}</span><br>
+<span>${todo.duedate}</span><br>
+<label for = "${todo.id}">Checkbox</label>
+<input id = "${todo.id}" type = "checkbox"><br>
+<button>Delete</button>
+`
+		list.append(node);
+	}
 
 	form.addEventListener('submit', event => {
 		event.preventDefault();
 		const title = document.getElementById('title').value;
 		const description = document.getElementById('description').value
 		const duedate = document.getElementById('duedate').value
-		const priority = document.getElementById('priority').value
-		if (title === '' || description === '' || duedate === '' || priority === '') {
+
+		if (title === '' || description === '' || duedate === '') {
 			alert("Enter all fields")
 		}
 		else {
-			const newTodo = new Todo(title, description, duedate, priority)
+			const newTodo = new todo(title, description, duedate)
 			todoItems.push(newTodo);
-			console.log(todoItems)
 			clearField()
 		}
+		displayData(todoItems);
 	})
 
 

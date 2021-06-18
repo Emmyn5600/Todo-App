bbc87152e703b0fd979b3be19019fb2e70b3acc9
emmyn5600 <emmy6618@gmail.com>
2021-06-18 10:49:21 +0200

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
 
 module.exports = {
-	entry: {
-		index: './src/index.js',
-		print: './src/print.js',
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+mode: 'development', 
+entry: {
+index: './src/index.js',
+print: './src/print.js',
+},
+output: {
+filename: '[name].bundle.js',
+path: path.resolve(__dirname, 'dist'),
+
+},
+module: {
+rules: [
+{
+test: /\.css$/i,
+use: ['style-loader', 'css-loader'],
+},
+],
+},
 };

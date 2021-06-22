5cac1318870a40994ff18e3f9d89b77ee6c18b50
emmyn5600 <emmy6618@gmail.com>
2021-06-22 15:42:54 +0200

    Work on localstorage

--- dist/print.bundle.js
+++ dist/print.bundle.js
@@ -8,59 +8,7 @@
  */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/print.js":
-/*!**********************!*\
-  !*** ./src/print.js ***!
-  \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ displayData)\n/* harmony export */ });\nfunction displayData(todo) {\n\tconst list = document.getElementById('content');\n\tconst item = document.querySelector(`[data-key='${todo.id}']`);\n\tif (todo.deleted) {\n\t\titem.remove();\n\t\treturn;\n\t}\n\n\tconst isChecked = todo.checked ? 'done' : '';\n\tconst node = document.createElement('li');\n\tnode.setAttribute('class', `todo-item ${isChecked}`);\n\tnode.setAttribute('data-key', todo.id);\n\tif (todo.project === 'Defualt') {\n\t\tnode.innerHTML += `\n<span>Project: ${todo.project}</span>\n<span>Title:${todo.title}</span><br>\n<span>${todo.description}</span><br>\n<span>${todo.duedate}</span><br>\n<label for = \"${todo.id}\" class = 'js-tick'>Checkbox</label>\n<input id = \"${todo.id}\" type = \"checkbox\" ><br>\n<button class=\"delete-todo js-delete-todo\">Delete</button>\n`;\n\t} else {\n\t\tnode.innerHTML = `\n<span>Project: ${todo.project}</span>\n<span>Title:${todo.title}</span><br>\n<span>${todo.description}</span><br>\n<span>${todo.duedate}</span><br>\n<label for = \"${todo.id}\" class = 'js-tick'>Checkbox</label>\n<input id = \"${todo.id}\" type = \"checkbox\" ><br>\n<button class=\"delete-todo js-delete-todo\">Delete</button>`;\n\t}\n\tif (item) {\n\t\tlist.replaceChild(node, item);\n\t} else {\n\t\tlist.append(node);\n\t}\n}\n\n\n//# sourceURL=webpack://todo-app/./src/print.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
 /******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/print.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;

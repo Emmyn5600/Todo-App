9d93b400ec22115393c4f49e6607cbef695afd16
emmyn5600 <emmy6618@gmail.com>
2021-06-22 16:48:33 +0200

    Fix the eslint errors

--- dist/print.bundle.js
+++ dist/print.bundle.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n\n//# sourceURL=webpack://todo-app/./src/print.js?");
+eval("\n\n//# sourceURL=webpack://todo-app/./src/print.js?");
 
 /***/ })
 
